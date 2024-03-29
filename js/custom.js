ba7aec7973577518afb00beca0945c9304ce20ab
Ahmed El Sayed <127688764+Gratz1@users.noreply.github.com>
2024-03-29 16:37:35 +0200

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,311 @@
+/* JS Document */
+
+/******************************
+
+[Table of Contents]
+
+1. Vars and Inits
+2. Set Header
+3. Init Menu
+4. InitDeptSlider
+5. Init Accordions
+
+
+******************************/
+
+$(document).ready(function()
+{
+	"use strict";
+
+	/* 
+
+	1. Vars and Inits
+
+	*/
+
+	var header = $('.header');
+	var menu = $('.menu');
+	var menuActive = false;
+
+	setHeader();
+
+	$(window).on('resize', function()
+	{
+		setHeader();
+
+		setTimeout(function()
+		{
+			$(window).trigger('resize.px.parallax');
+		}, 375);
+	});
+
+	$(document).on('scroll', function()
+	{
+		setHeader();
+	});
+
+	initMenu();
+	initDeptSlider();
+	initAccordions();
+
+	/* 
+
+	2. Set Header
+
+	*/
+
+	function setHeader()
+	{
+		if($(window).scrollTop() > 91)
+		{
+			header.addClass('scrolled');
+		}
+		else
+		{
+			header.removeClass('scrolled');
+		}
+	}
+
+	/* 
+
+	3. Init Menu
+
+	*/
+
+	function initMenu()
+	{
+		if($('.hamburger').length && $('.menu').length)
+		{
+			var hamb = $('.hamburger');
+			var close = $('.menu_close_container');
+
+			hamb.on('click', function()
+			{
+				if(!menuActive)
+				{
+					openMenu();
+				}
+				else
+				{
+					closeMenu();
+				}
+			});
+
+			close.on('click', function()
+			{
+				if(!menuActive)
+				{
+					openMenu();
+				}
+				else
+				{
+					closeMenu();
+				}
+			});
+
+	
+		}
+	}
+
+	function openMenu()
+	{
+		menu.addClass('active');
+		menuActive = true;
+	}
+
+	function closeMenu()
+	{
+		menu.removeClass('active');
+		menuActive = false;
+	}
+
+	/* 
+
+	4. Init Dept Slider
+
+	*/
+
+	function initDeptSlider()
+	{
+		if($('.dept_slider').length)
+		{
+			var deptSlider = $('.dept_slider');
+			deptSlider.owlCarousel(
+			{
+				items:4,
+				autoplay:true,
+				loop:true,
+				nav:false,
+				dots:false,
+				margin:30,
+				smartSpeed:1200,
+				responsive:
+				{
+					0:{items:1},
+					768:{items:2},
+					992:{items:3},
+					1200:{items:4}
+				}
+			});
+
+			if($('.dept_slider_nav').length)
+			{
+				var next = $('.dept_slider_nav');
+				next.on('click', function()
+				{
+					deptSlider.trigger('next.owl.carousel');
+				});
+			}
+            
+            
+            
+            
+            
+            
+            
+          /*============ fade ==============*/  
+            
+            
+            $(window).on("load",function() {
+  $(window).scroll(function() {
+    var windowBottom = $(this).scrollTop() + $(this).innerHeight();
+    $(".fade").each(function() {
+      /* Check the location of each desired element */
+      var objectBottom = $(this).offset().top + $(this).outerHeight();
+      
+      /* If the element is completely within bounds of the window, fade it in */
+      if (objectBottom < windowBottom) { //object comes into view (scrolling down)
+        if ($(this).css("opacity")==0) {$(this).fadeTo(500,1);}
+      } else { //object goes out of view (scrolling up)
+        if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
+      }
+    });
+  }).scroll(); //invoke scroll-handler on page-load
+});
+		}
+	}
+
+	/* 
+
+	5. Init Accordions
+
+	*/
+
+	function initAccordions()
+	{
+		if($('.accordion').length)
+		{
+			var accs = $('.accordion');
+
+			accs.each(function()
+			{
+				var acc = $(this);
+
+				if(acc.hasClass('active'))
+				{
+					var panel = $(acc.next());
+					var panelH = panel.prop('scrollHeight') + "px";
+					
+					if(panel.css('max-height') == "0px")
+					{
+						panel.css('max-height', panel.prop('scrollHeight') + "px");
+					}
+					else
+					{
+						panel.css('max-height', "0px");
+					} 
+				}
+
+				acc.on('click', function()
+				{
+					if(acc.hasClass('active'))
+					{
+						acc.removeClass('active');
+						var panel = $(acc.next());
+						var panelH = panel.prop('scrollHeight') + "px";
+						
+						if(panel.css('max-height') == "0px")
+						{
+							panel.css('max-height', panel.prop('scrollHeight') + "px");
+						}
+						else
+						{
+							panel.css('max-height', "0px");
+						} 
+					}
+					else
+					{
+						acc.addClass('active');
+						var panel = $(acc.next());
+						var panelH = panel.prop('scrollHeight') + "px";
+						
+						if(panel.css('max-height') == "0px")
+						{
+							panel.css('max-height', panel.prop('scrollHeight') + "px");
+						}
+						else
+						{
+							panel.css('max-height', "0px");
+						} 
+					}
+				});
+			});
+		}
+	}
+
+});
+
+
+
+
+/*===================== slide =================*/
+$(function() {
+  var $blocks = $('.animBlock.notViewed');
+  var $window = $(window);
+
+  $window.on('scroll', function(e){
+    $blocks.each(function(i,elem){
+      if($(this).hasClass('viewed')) 
+        return;
+        
+      isScrolledIntoView($(this));
+    });
+  });
+});
+
+function isScrolledIntoView(elem) {
+  var docViewTop = $(window).scrollTop();
+  var docViewBottom = docViewTop + $(window).height();
+  var elemOffset = 0;
+  
+  if(elem.data('offset') != undefined) {
+    elemOffset = elem.data('offset');
+  }
+  var elemTop = $(elem).offset().top;
+  var elemBottom = elemTop + $(elem).height();
+  
+  if(elemOffset != 0) { // custom offset is updated based on scrolling direction
+    if(docViewTop - elemTop >= 0) {
+      // scrolling up from bottom
+      elemTop = $(elem).offset().top + elemOffset;
+    } else {
+      // scrolling down from top
+      elemBottom = elemTop + $(elem).height() - elemOffset
+    }
+  }
+  
+  if((elemBottom <= docViewBottom) && (elemTop >= docViewTop)) {
+    // once an element is visible exchange the classes
+    $(elem).removeClass('notViewed').addClass('viewed');
+    
+    var animElemsLeft = $('.animBlock.notViewed').length;
+    if(animElemsLeft == 0){
+      // with no animated elements left debind the scroll event
+      $(window).off('scroll');
+    }
+  }
+}
+
+
+
+
